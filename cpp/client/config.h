263a3d1b7d4d370b47554952d1f5763c3b142bcb
songmuheo <kmo2578@gmail.com>
2024-07-19 19:03:28 +0900

    update

--- cpp/client/config.h
+++ cpp/client/config.h
@@ -8,6 +8,6 @@ const int SERVER_PORT = 12345;
 const int PACKET_SIZE = 1500;
 const float PACKET_INTERVAL = 0.033;
 const char* INTERFACE1_IP = "10.16.130.64";
-const char* INTERFACE2_IP = "10.16.130.84";
+const char* INTERFACE2_IP = "10.16.132.84";
 
 #endif // CONFIG_H
--- python/client/config.py
+++ python/client/config.py
@@ -5,4 +5,4 @@ SERVER_PORT = 12345  # 서버 포트
 PACKET_SIZE = 1500  # 패킷 크기 (H.264 코덱의 일반적인 프레임 크기)
 PACKET_INTERVAL = 0.033  # 패킷 간 간격 (30fps 영상 데이터 전송 간격)
 INTERFACE1_IP = "10.16.130.64"  # 첫 번째 Wi-Fi 인터페이스 IP
-INTERFACE2_IP = "10.16.130.84"  # 두 번째 Wi-Fi 인터페이스 IP
+INTERFACE2_IP = "10.16.132.84"  # 두 번째 Wi-Fi 인터페이스 IP
