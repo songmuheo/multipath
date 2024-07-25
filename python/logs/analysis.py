609a719283d52e234e065a71839d4d22663d3a4d
songmuheo <kmo2578@gmail.com>
2024-07-25 18:43:32 +0900

    python

--- python/logs/analysis.py
+++ python/logs/analysis.py
@@ -20,7 +20,7 @@ latency_difference = merged_df[~merged_df['Latency_1'].isna() & ~merged_df['Late
 latency_difference['Latency Difference'] = abs(latency_difference['Latency_1'] - latency_difference['Latency_2'])
 
 # 차이가 큰 패킷만 필터링 (예: Latency 차이가 0.1초 (100ms) 이상인 경우)
-significant_latency_diff = latency_difference[latency_difference['Latency Difference'] > 0.1]
+significant_latency_diff = latency_difference[latency_difference['Latency Difference'] > 0.02]
 
 # 누락된 Sequence Number 분석
 all_sequence_numbers = set(range(int(df['Sequence Number'].min()), int(df['Sequence Number'].max()) + 1))
@@ -32,20 +32,20 @@ missing_sequence_numbers_1 = sorted(all_sequence_numbers - received_sequence_num
 missing_sequence_numbers_2 = sorted(all_sequence_numbers - received_sequence_numbers_2)
 
 # 결과 출력
-print("1번 인터페이스에서 수신되지 않은 패킷:")
-print(missing_1)
+# print("1번 인터페이스에서 수신되지 않은 패킷:")
+# print(missing_1)
 
-print("\n2번 인터페이스에서 수신되지 않은 패킷:")
-print(missing_2)
+# print("\n2번 인터페이스에서 수신되지 않은 패킷:")
+# print(missing_2)
 
-print("\nLatency 차이가 큰 패킷:")
-print(significant_latency_diff)
+# print("\nLatency 차이가 큰 패킷:")
+# print(significant_latency_diff)
 
-print("\n1번 인터페이스에서 누락된 Sequence Number:")
-print(missing_sequence_numbers_1)
+# print("\n1번 인터페이스에서 누락된 Sequence Number:")
+# print(missing_sequence_numbers_1)
 
-print("\n2번 인터페이스에서 누락된 Sequence Number:")
-print(missing_sequence_numbers_2)
+# print("\n2번 인터페이스에서 누락된 Sequence Number:")
+# print(missing_sequence_numbers_2)
 
 # 결과 저장
 missing_1.to_csv('/home/songmu/Multipath/python/logs/missing_1.csv', index=False)
