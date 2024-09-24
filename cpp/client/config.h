703c0f2169a5b1ea9e61c22f5caec25da5f82167
songmuheo <kmo2578@gmail.com>
2024-09-24 15:51:41 +0900

    temp

--- cpp/client/config.h
+++ cpp/client/config.h
@@ -13,8 +13,8 @@ const float PACKET_INTERVAL = 0.033;
 const int FPS = 30;
 const int BITRATE = 1500000;
 
-// const std::string BASE_FILEPATH = "/home/widen/"
-const std::string BASE_FILEPATH = "/home/songmu/";
+const std::string BASE_FILEPATH = "/home/widen/"
+// const std::string BASE_FILEPATH = "/home/songmu/";
 
 // File path to save png files
 const std::string SAVE_FILEPATH = BASE_FILEPATH + "Multipath/cpp/results/client/";
